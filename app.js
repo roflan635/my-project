0f46fa92556458593a43d9d047bb2db4ed5c6930
roflan635 <nikolaev.nikita2281337@gmail.com>
2025-12-12 13:30:42 +0400

    1 этап задания 4 завершен

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
-  
+
+var app = express()
+app.use(cookieParser());
+
 const connection = mysql.createPool({
   host: "localhost",
   user: "anton",
@@ -11,7 +15,7 @@ const connection = mysql.createPool({
   port: 3306
 });
 
-const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "hbs");
@@ -32,15 +36,35 @@ app.post("/login", async (req, res) => {
   console.log("Логин:", username);
   console.log("Пароль:", password);
   const [rows] = await connection.query(
-    "SELECT * FROM user WHERE username=? AND password=?",
+    "SELECT id_us, username, password FROM user WHERE username=? AND password=?",
     [username, password]
   );
   if (rows.length > 0) {
-    res.send("Авторизация успешна!");
+    const userId = rows[0].id_us;
+    res.cookie('userId', userId);
+    res.send("Авторизация успешна!"); 
+    res.redirect("/profile");
   } else {
     res.send("Неверный логин или пароль");
   }
 });
+
+app.get("/profile", async (req, res) => {
+  const userId = req.cookies.userId;
+    // Получаем данные пользователя по ID
+    const [rows] = await connection.query(
+      "SELECT id_us, username, password, login, phone_number FROM user WHERE id_us = ?",
+      [userId]
+    );
+      if (rows.length > 0) {
+      const user = rows[0];
+      res.render("profile", { 
+        username: user.username,
+        password: user.password,
+        login: user.login,
+        phone_number: user.phone_number
+      })};
+  });
 
 app.listen(3000, () => {
   console.log("Сервер запущен на http://localhost:3000");
